32190a144d4f1f047b3f5888893f1b92b7231f59
John Torres <jtorres0802@gmail.com>
2019-09-26 11:27:48 -0400

    Commit prior to reboot.

--- server/app.js
+++ server/app.js
@@ -4,8 +4,8 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var hbs = require("hbs");
+const cors = require("cors");
 
-var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var bandsRouter = require("./routes/bands");
 
@@ -20,8 +20,6 @@ app.use(function(req, res, next) {
   );
   next();
 });
-
-const cors = require("cors");
 
 app.use(
   cors({
@@ -43,7 +41,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/bands", bandsRouter);
 
